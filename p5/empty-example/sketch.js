d29f6b96d5a8d6107beff4a29b57e2787c258c68
bcecil2 <blakemadness@gmail.com>
2017-10-19 15:28:57 -0700

    added circles

---
+++ p5/empty-example/sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(480,120);
+}
+
+function draw() {
+  if(mouseIsPressed) {
+    fill(1);
+  } else {
+    fill(225);
+  } 
+    ellipse(mouseX,mouseY,80,80);
+}
---
+++ p5/firstproject/practice1.js
@@ -0,0 +1,20 @@
+function setup() {
+createCanvas(480, 120);
+strokeWeight(4);
+}
+
+function draw(){
+    background(0);
+     fill(201);
+     line(120,60,320,90);
+     fill(225);
+     rect(145,34,50,15);
+     fill(240);
+     ellipse(240,60,30,90);
+    
+    
+}
+
+
+
+
